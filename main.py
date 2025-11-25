ed35f4bd362b2524edd11f2ec23407ca8cd55d2c
Dhyan Patoliya <70569825+DhyanCanPlay@users.noreply.github.com>
2025-11-25 11:09:49 +0000

    Initialize car park grid and user options

---
+++ main.py
@@ -0,0 +1,21 @@
+grid = [['-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
+        ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
+        ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
+        ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
+        ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
+        ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-']
+         ]  
+print(grid)
+   
+    
+
+print("1. reset all spaces in the car park  to empty ")
+print("2. remove a car")
+print("3. add v car")
+print("4. Display the car park grid")
+print("5. Quit ")
+print("select an option")
+option = int(input("Enter a choice(1-5):  "))
+
+
+  

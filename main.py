ffd0a57ddb56a3ca7a48f9224e621173cf3dcd80
Dhyan Patoliya <70569825+DhyanCanPlay@users.noreply.github.com>
2025-11-26 13:29:16 +0000

    Refactor menu and options functions in main.py

--- main.py
+++ main.py
@@ -5,17 +5,52 @@ grid = [['-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
         ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
         ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-']
          ]  
-print(grid)
-   
+
+def options (x):
+    if x == 1:
+        grid = [['-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
+                ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
+                ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
+                ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
+                ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
+                ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-']]
+        print("Reset Success")
+        menu()
+        
+        
+    elif x == 2:
+        print("-----CAR REMOVE-----")
+        rmcarRow = input("Enter row of the car: ")
+        rmcarCol = input("Enter col of the car: ")
+        menu()
+    elif x == 3:
+        print("exeuted option 3")
+        menu()
+    elif x == 4:
+        
+        for row in grid:
+            print(" | ".join(row)) 
+            print("   ")
+        menu()
+    elif x == 5:
+        print("exeuted option 5")
+        menu()
+    
     
 
-print("1. reset all spaces in the car park  to empty ")
-print("2. remove a car")
-print("3. add v car")
-print("4. Display the car park grid")
-print("5. Quit ")
-print("select an option")
-option = int(input("Enter a choice(1-5):  "))
 
 
-  
+
+def menu():
+    print("--------------- MENU ---------------")
+    print("1. reset all spaces in the car park  to empty ")
+    print("2. remove a car")
+    print("3. add a car")
+    print("4. Display the car park grid")
+    print("5. Quit ")
+    print("select an option")
+    option = int(input("Enter a choice(1-5):  "))
+    options(option)
+    
+
+menu()
